5119d414e2110f99cc8c0db28928de45d453f666
Hasanovic32 <78445358+Hasanovic32@users.noreply.github.com>
2024-09-26 22:47:10 +0200

    update server

--- backend/server.js
+++ backend/server.js
@@ -75,3 +75,7 @@ const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
